a2118e39ddbb3be644d870ddf51c9b0c5caf7e57
Alejvz <86327633+alejvz@users.noreply.github.com>
2023-05-16 15:19:24 -0500

    commit 7

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -54,8 +54,8 @@ const Dashboard = () => {
   return (
     <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Imágenes creadas por nuestra comunidad</h1>
-        <p className="mt-2 text-[#666E75] text-[16px] max-w-[500px]">Explore una colección de imágenes imaginativas y visualmente impresionantes generadas por Inteligencia Artificial</p>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Diseños creadas por nuestra comunidad</h1>
+        <p className="mt-2 text-[#666E75] text-[16px] max-w-[500px]">Explore una colección de diseños imaginativas y visualmente impresionantes generadas por Inteligencia Artificial</p>
       </div>
       <div className="mt-16">
         <FormField
