75771c622965139f987a7dc73d256d7ff572ff20
Alejvz <86327633+alejvz@users.noreply.github.com>
2023-05-16 13:22:59 -0500

    commit 4

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -13,7 +13,7 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
             onClick={handleSurpriseMe}
             className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
           >
-            Surprise me
+           Sorpréndeme 
           </button>
         }
       </div>
